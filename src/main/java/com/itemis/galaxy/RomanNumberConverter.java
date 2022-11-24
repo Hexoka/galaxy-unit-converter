32bb6c2e7ae13c343ec5493133c0e6c9b509c234
Hexoka <robinschluensen@gmx.de>
2022-11-24 14:22:36 +0100

    Split up RomanNumberConverter test case
    Split up RomanNumberConverter test case into single tests

--- src/main/java/com/itemis/galaxy/RomanNumberConverter.java
+++ src/main/java/com/itemis/galaxy/RomanNumberConverter.java
@@ -2,6 +2,7 @@ package com.itemis.galaxy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.OptionalInt;
 
 /**
@@ -11,10 +12,79 @@ public final class RomanNumberConverter {
 
 	/**
 	 * Calculate the number for a roman numeral.
-	 * @param romanSymbols roman numeral (must not contain null values)
+	 * @param romanSymbols roman numeral
 	 * @return calculated number or empty if the number can not be calculated because of an invalid input
 	 */
 	public OptionalInt calculateNumber(List<RomanSymbol> romanSymbols) {
+		if(romanSymbols.size() < 0) {
+			return OptionalInt.of(0);
+		}
+		int totalValue = 0;
+		int lastSplitIndex = 0;
+		int maxValue = 1000;
+		int minValue = 100;
+		for(int i = 0; i < romanSymbols.size(); i++) {
+			int value = romanSymbols.get(i).getValue();
+			if(value < maxValue && value >= minValue) {
+				OptionalInt digitValue = calculateDigitValue(romanSymbols.subList(lastSplitIndex, i));
+				if(digitValue.isEmpty()) {
+					return OptionalInt.empty();
+				}
+				maxValue /= 10;
+				minValue /= 10;
+				totalValue += digitValue.getAsInt();
+				lastSplitIndex = i;
+			} else if(value < minValue) {
+				maxValue /= 10;
+				minValue /= 10;
+				i--;
+			}
+		}
+		OptionalInt digitValue = calculateDigitValue(romanSymbols.subList(lastSplitIndex, romanSymbols.size()));
+		if(digitValue.isEmpty()) {
+			return OptionalInt.empty();
+		}
+		totalValue += digitValue.getAsInt();
+		
+		return OptionalInt.of(totalValue);
+		
+//		boolean valid = checkValidity(romanSymbols);
+//		if(!valid) {
+//			return OptionalInt.empty();
+//		}
+//		return calculateNumberForValid(romanSymbols);
+	}
+	
+	private OptionalInt calculateDigitValue(List<RomanSymbol> romanSymbols) {
+		System.out.print("Digit: ");
+		romanSymbols.stream().map(Enum::name).forEach(System.out::print);
+		System.out.println();
+		
+		if(romanSymbols.size() == 2) {
+			if(romanSymbols.get(1).getValue() > romanSymbols.get(0).getValue()) {
+				System.out.println("Subtraction: "  +romanSymbols.get(0).name() + romanSymbols.get(1).name());
+				return calculateSubtraction(romanSymbols.get(0), romanSymbols.get(1));
+			}
+		}
+		
+		int value = 0;
+		RomanSymbol lastSymbol = RomanSymbol.M;
+		for(RomanSymbol symbol : romanSymbols) {
+			if(symbol.getValue() > lastSymbol.getValue()) {
+				return OptionalInt.empty();
+			}
+			value += symbol.getValue();
+			lastSymbol = symbol;
+		}
+		return OptionalInt.of(value);
+	}
+
+	/**
+	 * Calculate the number for a roman numeral.
+	 * @param romanSymbols roman numeral (must be a valid numeral)
+	 * @return calculated number or empty if the number can not be calculated because of an invalid input
+	 */
+	private OptionalInt calculateNumberForValid(List<RomanSymbol> romanSymbols) {
 		romanSymbols = new ArrayList<>(romanSymbols);
 		switch(romanSymbols.size()) {
 		case 0:
@@ -28,8 +98,8 @@ public final class RomanNumberConverter {
 		if(first.getValue() >= second.getValue()) {
 			//Split the calculations after the first symbol
 			romanSymbols.remove(0);
-			OptionalInt valueFirst = calculateNumber(List.of(first));
-			OptionalInt valueWithoutFirst = calculateNumber(romanSymbols);
+			OptionalInt valueFirst = calculateNumberForValid(List.of(first));
+			OptionalInt valueWithoutFirst = calculateNumberForValid(romanSymbols);
 			if(valueFirst.isPresent() && valueWithoutFirst.isPresent()) {
 				int value = valueWithoutFirst.getAsInt() + valueFirst.getAsInt();
 				return OptionalInt.of(value);
@@ -41,13 +111,61 @@ public final class RomanNumberConverter {
 			romanSymbols.remove(0);
 			romanSymbols.remove(0);
 			OptionalInt valueFirstSecond = calculateSubtraction(first, second);
-			OptionalInt valueWithoutFirst = calculateNumber(romanSymbols);
+			OptionalInt valueWithoutFirst = calculateNumberForValid(romanSymbols);
 			if(valueFirstSecond.isPresent() && valueWithoutFirst.isPresent()) {
 				int value = valueWithoutFirst.getAsInt() + valueFirstSecond.getAsInt();
 				return OptionalInt.of(value);
 			} else {
 				return OptionalInt.empty();
 			}
+		}
+	}
+	
+	
+
+	private boolean checkValidity(List<RomanSymbol> romanSymbols) {
+		//Check for null values
+		boolean hasNullValues = romanSymbols.stream()
+											.filter(Objects::isNull)
+											.findAny()
+											.isPresent();
+		if(hasNullValues) {
+			return false;
+		}
+		
+		//Check disallowed repeats of same symbol
+		int count = 0;
+		RomanSymbol lastSymbol = null;
+		for(RomanSymbol symbol : romanSymbols) {
+			if(symbol == lastSymbol) {
+				count++;
+			} else {
+				count = 1;
+				lastSymbol = symbol;
+			}
+			if(count > allowedRepeats(lastSymbol)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+
+
+	private int allowedRepeats(RomanSymbol symbol) {
+		switch(symbol) {
+		case I:
+		case X:
+		case C:
+		case M:
+			return 3;
+		case D:
+		case L:
+		case V:
+		default:
+			return 1;
+			
 		}
 	}
 
--- src/test/java/com/itemis/galaxy/RomanNumberConverterTest.java
+++ src/test/java/com/itemis/galaxy/RomanNumberConverterTest.java
@@ -1,6 +1,7 @@
 package com.itemis.galaxy;
 
 import java.util.List;
+import java.util.OptionalInt;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,196 +10,304 @@ import org.junit.Test;
  * This class tests the {@link RomanNumberConverter} class.
  */
 public final class RomanNumberConverterTest {
-	
+
+	private final RomanNumberConverter converter = new RomanNumberConverter();
+
 	/**
-	 * Tests the method {@link RomanNumberConverter#calculateNumber(List)}
-	 * Uses an empty list.
+	 * Tests the method {@link RomanNumberConverter#calculateNumber(List)} Uses an
+	 * empty list.
 	 */
 	@Test
-	public void testCalculateNumber() {
-		RomanNumberConverter converter = new RomanNumberConverter();
-		
+	public void testCalculateNumber0() {
 		List<RomanSymbol> input = List.of();
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(0, converter.calculateNumber(input).getAsInt());
-		
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(0, result.getAsInt());
 	}
 
 	/**
-	 * Tests the method {@link RomanNumberConverter#calculateNumber(List)}
-	 * Uses only single roman symbols.
+	 * Tests the method {@link RomanNumberConverter#calculateNumber(List)} Uses only
+	 * single roman symbols.
 	 */
 	@Test
 	public void testCalculateNumber1() {
-		RomanNumberConverter converter = new RomanNumberConverter();
-		
 		List<RomanSymbol> input = List.of(RomanSymbol.I);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(1, converter.calculateNumber(input).getAsInt());
-		
-		input = List.of(RomanSymbol.V);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(5, converter.calculateNumber(input).getAsInt());
-		
-		input = List.of(RomanSymbol.X);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(10, converter.calculateNumber(input).getAsInt());
-		
-		input = List.of(RomanSymbol.L);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(50, converter.calculateNumber(input).getAsInt());
-		
-		input = List.of(RomanSymbol.C);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(100, converter.calculateNumber(input).getAsInt());
-		
-		input = List.of(RomanSymbol.D);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(500, converter.calculateNumber(input).getAsInt());
-		
-		input = List.of(RomanSymbol.M);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(1000, converter.calculateNumber(input).getAsInt());
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(1, result.getAsInt());
 	}
-	
 
-	
-	/**
-	 * Tests the method {@link RomanNumberConverter#calculateNumber(List)}
-	 * Uses combinations of two roman symbols
-	 */
 	@Test
 	public void testCalculateNumber2() {
-		RomanNumberConverter converter = new RomanNumberConverter();
-		
-		List<RomanSymbol> input = List.of(RomanSymbol.I, RomanSymbol.I);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(2, converter.calculateNumber(input).getAsInt());
-
-		input = List.of(RomanSymbol.I, RomanSymbol.V);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(4, converter.calculateNumber(input).getAsInt());
-
-		input = List.of(RomanSymbol.V, RomanSymbol.I);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(6, converter.calculateNumber(input).getAsInt());
-
-		input = List.of(RomanSymbol.I, RomanSymbol.X);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(9, converter.calculateNumber(input).getAsInt());
-
-		input = List.of(RomanSymbol.X, RomanSymbol.I);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(11, converter.calculateNumber(input).getAsInt());
-
-		input = List.of(RomanSymbol.X, RomanSymbol.V);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(15, converter.calculateNumber(input).getAsInt());
-		
-		input = List.of(RomanSymbol.X, RomanSymbol.L);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(40, converter.calculateNumber(input).getAsInt());
-		
-		input = List.of(RomanSymbol.X, RomanSymbol.C);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(90, converter.calculateNumber(input).getAsInt());
-
-		input = List.of(RomanSymbol.X, RomanSymbol.X);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(20, converter.calculateNumber(input).getAsInt());
-		
-		input = List.of(RomanSymbol.C, RomanSymbol.D);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(400, converter.calculateNumber(input).getAsInt());
-
-		input = List.of(RomanSymbol.C, RomanSymbol.C);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(200, converter.calculateNumber(input).getAsInt());
-		
-		input = List.of(RomanSymbol.C, RomanSymbol.M);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(900, converter.calculateNumber(input).getAsInt());
-
-		input = List.of(RomanSymbol.M, RomanSymbol.M);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(2000, converter.calculateNumber(input).getAsInt());
+		List<RomanSymbol> input = List.of(RomanSymbol.V);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(5, result.getAsInt());
 	}
-	
-	/**
-	 * Tests the method {@link RomanNumberConverter#calculateNumber(List)}
-	 * Uses combinations of more than two roman symbols
-	 */
+
 	@Test
 	public void testCalculateNumber3() {
-		RomanNumberConverter converter = new RomanNumberConverter();
-
-		List<RomanSymbol> input = List.of(RomanSymbol.I, RomanSymbol.I, RomanSymbol.I);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(3, converter.calculateNumber(input).getAsInt());
-
-		input = List.of(RomanSymbol.V, RomanSymbol.I, RomanSymbol.I);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(7, converter.calculateNumber(input).getAsInt());
-
-		input = List.of(RomanSymbol.V, RomanSymbol.I, RomanSymbol.I, RomanSymbol.I);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(8, converter.calculateNumber(input).getAsInt());
-
-		input = List.of(RomanSymbol.X, RomanSymbol.I, RomanSymbol.V);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(14, converter.calculateNumber(input).getAsInt());
-		
-		input = List.of(RomanSymbol.M, RomanSymbol.C, RomanSymbol.M, RomanSymbol.X, RomanSymbol.L, RomanSymbol.I, RomanSymbol.V);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(1944, converter.calculateNumber(input).getAsInt());
-		
-		input = List.of(RomanSymbol.M, RomanSymbol.C, RomanSymbol.M, RomanSymbol.I, RomanSymbol.I, RomanSymbol.I);
-		Assert.assertTrue(converter.calculateNumber(input).isPresent());
-		Assert.assertEquals(1903, converter.calculateNumber(input).getAsInt());
+		List<RomanSymbol> input = List.of(RomanSymbol.X);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(10, result.getAsInt());
 	}
-	
-	/**
-	 * Tests the method {@link RomanNumberConverter#calculateNumber(List)}
-	 * Uses invalid combinations of multiple roman symbols
-	 */
+
 	@Test
 	public void testCalculateNumber4() {
-		RomanNumberConverter converter = new RomanNumberConverter();
-		
+		List<RomanSymbol> input = List.of(RomanSymbol.L);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(50, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber5() {
+		List<RomanSymbol> input = List.of(RomanSymbol.C);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(100, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber6() {
+		List<RomanSymbol> input = List.of(RomanSymbol.D);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(500, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber7() {
+		List<RomanSymbol> input = List.of(RomanSymbol.M);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(1000, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber8() {
+		List<RomanSymbol> input = List.of(RomanSymbol.I, RomanSymbol.I);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(2, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber9() {
+		List<RomanSymbol> input = List.of(RomanSymbol.I, RomanSymbol.V);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(4, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber10() {
+		List<RomanSymbol> input = List.of(RomanSymbol.V, RomanSymbol.I);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(6, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber11() {
+		List<RomanSymbol> input = List.of(RomanSymbol.I, RomanSymbol.X);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(9, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber12() {
+		List<RomanSymbol> input = List.of(RomanSymbol.X, RomanSymbol.I);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(11, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber13() {
+		List<RomanSymbol> input = List.of(RomanSymbol.X, RomanSymbol.V);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(15, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber14() {
+		List<RomanSymbol> input = List.of(RomanSymbol.X, RomanSymbol.L);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(40, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber15() {
+		List<RomanSymbol> input = List.of(RomanSymbol.X, RomanSymbol.C);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(90, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber16() {
+		List<RomanSymbol> input = List.of(RomanSymbol.X, RomanSymbol.X);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(20, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber17() {
+		List<RomanSymbol> input = List.of(RomanSymbol.C, RomanSymbol.D);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(400, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber18() {
+		List<RomanSymbol> input = List.of(RomanSymbol.C, RomanSymbol.C);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(200, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber19() {
+		List<RomanSymbol> input = List.of(RomanSymbol.C, RomanSymbol.M);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(900, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber20() {
+		List<RomanSymbol> input = List.of(RomanSymbol.M, RomanSymbol.M);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(2000, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber21() {
+		List<RomanSymbol> input = List.of(RomanSymbol.I, RomanSymbol.I, RomanSymbol.I);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(3, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber22() {
+		List<RomanSymbol> input = List.of(RomanSymbol.V, RomanSymbol.I, RomanSymbol.I);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(7, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber23() {
+		List<RomanSymbol> input = List.of(RomanSymbol.V, RomanSymbol.I, RomanSymbol.I, RomanSymbol.I);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(8, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber24() {
+		List<RomanSymbol> input = List.of(RomanSymbol.X, RomanSymbol.I, RomanSymbol.V);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(14, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber25() {
+		List<RomanSymbol> input = List.of(RomanSymbol.M, RomanSymbol.C, RomanSymbol.M, RomanSymbol.X, RomanSymbol.L,
+				RomanSymbol.I, RomanSymbol.V);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(1944, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber26() {
+		List<RomanSymbol> input = List.of(RomanSymbol.M, RomanSymbol.C, RomanSymbol.M, RomanSymbol.I, RomanSymbol.I,
+				RomanSymbol.I);
+		OptionalInt result = converter.calculateNumber(input);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(1903, result.getAsInt());
+	}
+
+	@Test
+	public void testCalculateNumber27() {
 		List<RomanSymbol> input = List.of(RomanSymbol.I, RomanSymbol.I, RomanSymbol.I, RomanSymbol.I);
 		Assert.assertFalse(converter.calculateNumber(input).isPresent());
-		
-		input = List.of(RomanSymbol.V, RomanSymbol.V);
-		Assert.assertFalse(converter.calculateNumber(input).isPresent());
-		
-		input = List.of(RomanSymbol.X, RomanSymbol.X, RomanSymbol.X, RomanSymbol.X);
-		Assert.assertFalse(converter.calculateNumber(input).isPresent());
-		
-		input = List.of(RomanSymbol.L, RomanSymbol.L);
-		Assert.assertFalse(converter.calculateNumber(input).isPresent());
-		
-		input = List.of(RomanSymbol.C, RomanSymbol.C, RomanSymbol.C, RomanSymbol.C);
-		Assert.assertFalse(converter.calculateNumber(input).isPresent());
-		
-		input = List.of(RomanSymbol.D, RomanSymbol.D);
-		Assert.assertFalse(converter.calculateNumber(input).isPresent());
-		
-		input = List.of(RomanSymbol.M, RomanSymbol.M, RomanSymbol.M, RomanSymbol.M);
-		Assert.assertFalse(converter.calculateNumber(input).isPresent());
-		
-		input = List.of(RomanSymbol.I, RomanSymbol.M);
-		Assert.assertFalse(converter.calculateNumber(input).isPresent());
-		
-		input = List.of(RomanSymbol.I, RomanSymbol.L);
-		Assert.assertFalse(converter.calculateNumber(input).isPresent());
-		
-		input = List.of(RomanSymbol.X, RomanSymbol.M);
-		Assert.assertFalse(converter.calculateNumber(input).isPresent());
-		
-		input = List.of(RomanSymbol.X, RomanSymbol.D);
-		Assert.assertFalse(converter.calculateNumber(input).isPresent());
+	}
 
-		input = List.of(RomanSymbol.X, RomanSymbol.I, RomanSymbol.V, RomanSymbol.I);
+	@Test
+	public void testCalculateNumber28() {
+		List<RomanSymbol> input = List.of(RomanSymbol.V, RomanSymbol.V);
+		Assert.assertFalse(converter.calculateNumber(input).isPresent());
+	}
+
+	@Test
+	public void testCalculateNumber29() {
+		List<RomanSymbol> input = List.of(RomanSymbol.X, RomanSymbol.X, RomanSymbol.X, RomanSymbol.X);
+		Assert.assertFalse(converter.calculateNumber(input).isPresent());
+	}
+
+	@Test
+	public void testCalculateNumber30() {
+		List<RomanSymbol> input = List.of(RomanSymbol.L, RomanSymbol.L);
+		Assert.assertFalse(converter.calculateNumber(input).isPresent());
+	}
+
+	@Test
+	public void testCalculateNumber31() {
+		List<RomanSymbol> input = List.of(RomanSymbol.C, RomanSymbol.C, RomanSymbol.C, RomanSymbol.C);
+		Assert.assertFalse(converter.calculateNumber(input).isPresent());
+	}
+
+	@Test
+	public void testCalculateNumber32() {
+		List<RomanSymbol> input = List.of(RomanSymbol.D, RomanSymbol.D);
+		Assert.assertFalse(converter.calculateNumber(input).isPresent());
+	}
+
+	@Test
+	public void testCalculateNumber33() {
+		List<RomanSymbol> input = List.of(RomanSymbol.M, RomanSymbol.M, RomanSymbol.M, RomanSymbol.M);
+		Assert.assertFalse(converter.calculateNumber(input).isPresent());
+	}
+
+	@Test
+	public void testCalculateNumber34() {
+		List<RomanSymbol> input = List.of(RomanSymbol.I, RomanSymbol.M);
+		Assert.assertFalse(converter.calculateNumber(input).isPresent());
+	}
+
+	@Test
+	public void testCalculateNumber35() {
+		List<RomanSymbol> input = List.of(RomanSymbol.I, RomanSymbol.L);
+		Assert.assertFalse(converter.calculateNumber(input).isPresent());
+	}
+
+	@Test
+	public void testCalculateNumber36() {
+		List<RomanSymbol> input = List.of(RomanSymbol.X, RomanSymbol.M);
+		Assert.assertFalse(converter.calculateNumber(input).isPresent());
+	}
+
+	@Test
+	public void testCalculateNumber37() {
+		List<RomanSymbol> input = List.of(RomanSymbol.X, RomanSymbol.D);
+		Assert.assertFalse(converter.calculateNumber(input).isPresent());
+	}
+
+	@Test
+	public void testCalculateNumber38() {
+		List<RomanSymbol> input = List.of(RomanSymbol.X, RomanSymbol.I, RomanSymbol.V, RomanSymbol.I);
 		Assert.assertFalse(converter.calculateNumber(input).isPresent());
 	}
 }
